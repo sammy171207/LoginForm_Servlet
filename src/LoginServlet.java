35583371763d33b7c1fb795e8854eaea59426242
sammy171207 <sameerrandive171207@gmail.com>
2024-03-09 22:31:51 +0530

    login_form using servlet and request dispatcher

---
+++ src/LoginServlet.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/loginserv")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		String username=request.getParameter("uname");
+		String pass= request.getParameter("password");
+		if(username.equals("admin")&& pass.equals("abc123")){
+			
+		 RequestDispatcher dispatcher= request.getRequestDispatcher("profile.html");
+		 dispatcher.forward(request, response);
+		}
+		else{
+			
+			
+			
+			
+		}
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
